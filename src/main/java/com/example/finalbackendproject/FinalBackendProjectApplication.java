08d3654f455199c031320c4ca0e419a88b4a276a
jasmijnslot <jasmijn.slot@novi-education.nl>
2023-07-27 13:19:13 +0200

    Initial commit

---
+++ src/main/java/com/example/finalbackendproject/FinalBackendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalbackendproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalBackendProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalBackendProjectApplication.class, args);
+    }
+
+}
